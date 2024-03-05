af9bab9caf539af0f674df2c87e548684f454419
Coko <91132775+Coko7@users.noreply.github.com>
2024-03-05 07:28:49 +0100

    fix: remove COLEMAK layer + remove OLED rot on offhand

--- keymap.c
+++ keymap.c
@@ -64,7 +64,7 @@
 enum sofle_layers {
     _DEFAULTS = 0,
     _QWERTY = 0,
-    _COLEMAK,
+    // _COLEMAK,
     _LOWER,
     _RAISE,
     _ADJUST,
@@ -74,7 +74,7 @@ enum sofle_layers {
 
 enum custom_keycodes {
     KC_QWERTY = SAFE_RANGE,
-    KC_COLEMAK,
+    // KC_COLEMAK,
     KC_LOWER,
     KC_RAISE,
     KC_ADJUST,
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,     XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                    KC_LGUI, KC_LALT, KC_LCTL, MO(2), KC_ENT,      KC_SPC,  MO(3),   KC_RCTL, KC_RALT, KC_RGUI
+                    KC_LGUI, KC_LALT, KC_LCTL, MO(1), KC_ENT,      KC_SPC,  MO(2),   KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 /*
@@ -121,13 +121,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
-[_COLEMAK] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
-  KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                              KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_BSPC,
-  KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                              KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,         XXXXXXX, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                    KC_LGUI, KC_LALT, KC_LCTL, TL_LOWR, KC_ENT,          KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
-),
+// [_COLEMAK] = LAYOUT(
+//   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
+//   KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                              KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_BSPC,
+//   KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                              KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT,
+//   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,         XXXXXXX, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+//                     KC_LGUI, KC_LALT, KC_LCTL, TL_LOWR, KC_ENT,          KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
+// ),
 
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
   _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
-                    _______, _______, _______, _______, _______,       _______, MO(4),   _______, _______, _______
+                    _______, _______, _______, _______, _______,       _______, MO(3),   _______, _______, _______
 ),
 
 /* RAISE
@@ -170,14 +170,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_INS,  KC_PSCR, KC_APP,  XXXXXXX, XXXXXXX,                         C(KC_LEFT), XXXXXXX, XXXXXXX, C(KC_RGHT), C(KC_BSPC), KC_BSPC,
   _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, KC_CAPS,                         KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT,    KC_DEL,     KC_BSPC,
   _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX, _______,       _______, KC_HOME,    KC_PGDN, KC_PGUP, KC_END,     XXXXXXX,    _______,
-                    _______, _______, _______, MO(4),   _______,       _______, _______,    _______, _______, _______
+                    _______, _______, _______, MO(3),   _______,       _______, _______,    _______, _______, _______
 ),
 
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    | RGB P| RGB B| RGB R|RGB SW|RGB SN| RGB K|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |QKBOOT|      |QWERTY|COLMAK| CGTOG|      |                    |      | RGBM-| RGBTg| RGBM+|      |      |
+ * |QKBOOT|      |      |      | CGTOG|      |                    |      | RGBM-| RGBTg| RGBM+|      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |ROMCLR|      | CGTOG|      |      |      |-------.    ,-------|      | VOL- | MUTE | VOL+ |      |      |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
@@ -188,11 +188,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_ADJUST] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX,                         RGB_M_P, RGB_M_B,  RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K,
-  QK_BOOT, XXXXXXX, KC_QWERTY, KC_COLEMAK, CG_TOGG, XXXXXXX,                         XXXXXXX, RGB_RMOD, RGB_TOG, RGB_MOD,  XXXXXXX,  XXXXXXX,
-  EE_CLR,  XXXXXXX, CG_TOGG,   XXXXXXX,    XXXXXXX, XXXXXXX,                         XXXXXXX, KC_VOLD,  KC_MUTE, KC_VOLU,  XXXXXXX,  XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, KC_MPRV,  KC_MPLY, KC_MNXT,  XXXXXXX,  XXXXXXX,
-                    _______,   _______,    _______, _______, _______,       _______, _______, _______, _______, _______
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         RGB_M_P, RGB_M_B,  RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K,
+  QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, CG_TOGG, XXXXXXX,                         XXXXXXX, RGB_RMOD, RGB_TOG, RGB_MOD,  XXXXXXX,  XXXXXXX,
+  EE_CLR,  XXXXXXX, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, KC_VOLD,  KC_MUTE, KC_VOLU,  XXXXXXX,  XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, KC_MPRV,  KC_MPLY, KC_MNXT,  XXXXXXX,  XXXXXXX,
+                    _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ) };
 
 #ifdef RGBLIGHT_ENABLE
@@ -279,12 +279,15 @@ void keyboard_post_init_user(void) {
 
 #ifdef OLED_ENABLE
 
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
+ static void render_logo(void) {
+     static const char PROGMEM qmk_logo[] = {
+         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+         0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+         0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+     };
+
+     oled_write_P(qmk_logo, false);
+
      // static const char PROGMEM raw_logo[] = {
      //     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,224,224,240,240,248,248,248,248,248,248,248,248,248,248,248,248,240,224,192,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,192,224,224,224,224,192,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,192,192,  0,  0,  0,  0,  0,  0,  0,  0,  0,
      //     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,255,255,255,255,255,255,255,127,127, 63, 63, 63, 63, 63, 63,255,255,255,255,255,255,255,254,  0,  0,  0,  0,  0,  0,  0,  0,  0,240,252,254, 14,  7,  7,  3,131,131,135,135,135,134,  0,  0,  0,112,112, 48, 48, 48,112,240,224,128,  0,  0,  0,243,243,  0,  0,  0,  0,240,240, 96,112, 48,112,240,224,192, 96,112, 48,112,240,240,192,  2,  2,198,230,246,118, 54, 54,118,246,230,198,  2,  2,  0,240,240, 96,112, 48, 48,112,243,227,  1,  0, 12, 78,102, 32, 32,  0,  0,  0,  0,
@@ -292,14 +295,14 @@ static void render_logo(void) {
      //     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  0, 19, 31, 31, 31, 15, 15, 31, 31, 31, 23,  0,  0,  1,  1,  0,  0,  0,  0,  0,  0,  0, 24, 28, 15,  5,  7, 13, 24, 28, 14, 12,  8,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
      // };
      // oled_write_raw_P(raw_logo, sizeof(raw_logo));
+ }
 
-    oled_write_P(qmk_logo, false);
-}
-
-static void print_status_narrow(void) {
+ static void print_status_narrow(void) {
      // Print current mode
      oled_write_P(PSTR("\n\n"), false);
-     oled_write_ln_P(PSTR("coko7\n-----"), false);
+
+     oled_write_ln_P(PSTR("coko7"), false);
+     oled_write_ln_P(PSTR("-----"), false);
 
      oled_write_ln_P(PSTR(""), false);
 
@@ -325,7 +328,7 @@ static void print_status_narrow(void) {
      oled_write_ln_P(PSTR("LAYER"), false);
 
      switch (get_highest_layer(layer_state)) {
-         case _COLEMAK:
+         // case _COLEMAK:
          case _QWERTY:
              // case _COLEMAKDH:
              oled_write_P(PSTR("Base\n"), false);
@@ -348,23 +351,30 @@ static void print_status_narrow(void) {
          default:
              oled_write_ln_P(PSTR("Undef"), false);
      }
-}
+ }
 
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    }
-    return rotation;
-}
+ // QMK function
+ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+     // if (is_keyboard_master()) {
+     //     return OLED_ROTATION_270;
+     // }
+     // if (!is_keyboard_master()) {
+     //     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+     // }
 
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        print_status_narrow();
-    } else {
-        render_logo();
-    }
-    return false;
-}
+     return rotation;
+ }
+
+
+ // QMK function
+ bool oled_task_user(void) {
+     if (is_keyboard_master()) {
+         print_status_narrow();
+     } else {
+         render_logo();
+     }
+     return false;
+ }
 
 #endif
 
@@ -375,11 +385,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                  set_single_persistent_default_layer(_QWERTY);
              }
              return false;
-         case KC_COLEMAK:
-             if (record->event.pressed) {
-                 set_single_persistent_default_layer(_COLEMAK);
-             }
-             return false;
+         // case KC_COLEMAK:
+         //     if (record->event.pressed) {
+         //         set_single_persistent_default_layer(_COLEMAK);
+         //     }
+         //     return false;
          // case KC_COLEMAKDH:
          //     if (record->event.pressed) {
          //         set_single_persistent_default_layer(_COLEMAKDH);
