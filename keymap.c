cf4a3498e946c97c4621233ce78b2b8cf4abd9de
Coko <91132775+Coko7@users.noreply.github.com>
2024-03-05 08:15:41 +0100

    feat(OLED): add display when CAPS lock

--- keymap.c
+++ keymap.c
@@ -351,9 +351,16 @@ void keyboard_post_init_user(void) {
          default:
              oled_write_ln_P(PSTR("Undef"), false);
      }
+
+     // Host Keyboard LED Status
+     led_t led_state = host_keyboard_led_state();
+     // oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+     oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+     // oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
  }
 
- // QMK function
+ // === QMK OLED API function ===
+ // Called at the start of oled_init, weak function overridable by the user
  oled_rotation_t oled_init_user(oled_rotation_t rotation) {
      // if (is_keyboard_master()) {
      //     return OLED_ROTATION_270;
@@ -366,7 +373,8 @@ void keyboard_post_init_user(void) {
  }
 
 
- // QMK function
+ // === QMK OLED API function ===
+ // Called at the start of oled_task, weak function overridable by the user
  bool oled_task_user(void) {
      if (is_keyboard_master()) {
          print_status_narrow();
